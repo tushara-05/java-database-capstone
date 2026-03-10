8b27249a0b83a4c58dce6236ebc7eac2e2e07b56
tushara-05 <tusharasasi05@gmail.com>
2026-03-10 22:31:21 +0530

    Update patientRecordRow.js

--- app/src/main/resources/static/js/components/patientRecordRow.js
+++ app/src/main/resources/static/js/components/patientRecordRow.js
@@ -5,13 +5,17 @@ export function createPatientRecordRow(patient) {
       <td class="patient-id">${patient.appointmentDate}</td>
       <td>${patient.id}</td>
       <td>${patient.patientId}</td>
-      <td><img src="../assets/images/addPrescriptionIcon/addPrescription.png" alt="addPrescriptionIcon" class="prescription-btn" data-id="${patient.id}"></img></td>
+      <td><img src="../assets/images/addPrescriptionIcon/addPrescription.png" alt="addPrescriptionIcon" class="prescription-btn" data-id="${patient.id}"></td>
     `;
 
   // Attach event listeners
-  tr.querySelector(".prescription-btn").addEventListener("click", () => {
-    window.location.href = `/pages/addPrescription.html?mode=view&appointmentId=${patient.id}`;
-  });
+  const prescriptionBtn = tr.querySelector(".prescription-btn");
+  if (prescriptionBtn) {
+    prescriptionBtn.addEventListener("click", () => {
+      window.location.href = `/pages/addPrescription.html?mode=view&appointmentId=${patient.id}`;
+    });
+  }
 
   return tr;
 }
+
