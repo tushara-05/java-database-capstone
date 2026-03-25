979546955efea22aa30d5737f41f9b6db4f2f1ca
tushara-05 <tusharasasi05@gmail.com>
2026-03-25 21:47:06 +0530

    Update appointmentRow.js

--- app/src/main/resources/static/js/components/appointmentRow.js
+++ app/src/main/resources/static/js/components/appointmentRow.js
@@ -1,19 +1,22 @@
 // appointmentRow.js
 export function getAppointments(appointment) {
   const tr = document.createElement("tr");
-
+  const isCompleted = appointment.status == 1;
+  const iconTitle = isCompleted ? "View Prescription" : "Add Prescription";
   tr.innerHTML = `
       <td class="patient-id">${appointment.patientName}</td>
       <td>${appointment.doctorName}</td>
       <td>${appointment.date}</td>
       <td>${appointment.time}</td>
-      <td><img src="../assets/images/edit/edit.png" alt="action" class="prescription-btn" data-id="${appointment.id}"></img></td>
+      <td><img src="../assets/images/edit/edit.png" alt="action" title="${iconTitle}" class="prescription-btn" data-id="${appointment.id}"></td>
     `;
-
   // Attach event listeners
-  tr.querySelector(".prescription-btn").addEventListener("click", () => {
-    window.location.href = `addPrescription.html?id=${patient.id}`;
-  });
-
+  const prescriptionBtn = tr.querySelector(".prescription-btn");
+  if (prescriptionBtn) {
+    prescriptionBtn.addEventListener("click", () => {
+      const modeParam = isCompleted ? "&mode=view" : "";
+      window.location.href = `/pages/addPrescription.html?appointmentId=${appointment.id}&patientName=${appointment.patientName}${modeParam}`;
+    });
+  }
   return tr;
 }
